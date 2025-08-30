7bdda6f9a26a6c889d8f73c959771299bc656a93
aryan.main27 <aryan.main27@gmail.com>
2025-08-30 10:30:54 +0530

    New Changes

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -19,16 +19,16 @@ int main()
   }
 
   // Decending Order
-  //   for (int i = 0; i < n; i++)
-  // {
-  //   for (int j = i + 1; j < n; j++)
-  //   {
-  //     if (a[i] < a[j])
-  //     {
-  //       swap(a[i], a[j]);
-  //     }
-  //   }
-  // }
+  for (int i = 0; i < n; i++)
+  {
+    for (int j = i + 1; j < n; j++)
+    {
+      if (a[i] < a[j])
+      {
+        swap(a[i], a[j]);
+      }
+    }
+  }
 
   for (int i = 0; i < n; i++)
   {
