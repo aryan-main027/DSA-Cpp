aa164ac2e804fa88310e275526fb78dc80e4532e
aryan.main27 <aryan.main27@gmail.com>
2025-08-30 14:18:32 +0530

    DSA

---
+++ Leetcode1608.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int specialArray(vector<int>& nums) {
+        int n = nums.size();
+        for (int X = 1; X <= n; ++X) {
+            // Count how many elements are >= X
+            int count = 0;
+            for (int num : nums) {
+                if (num >= X) {
+                    count++;
+                }
+            }
+            if (count == X) {
+                return X;
+            }
+        }
+        return -1;
+    }
+};
