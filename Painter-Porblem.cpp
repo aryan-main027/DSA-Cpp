c15f35e9aecc68461a9ca1f381a33c44fafc12f1
Aryan Shashwat <aryan.main27@gmail.com>
2025-09-02 18:22:59 +0530

    Add files via upload

---
+++ Painter-Porblem.cpp
@@ -0,0 +1,42 @@
+class Solution
+{
+public:
+  int minTime(vector<int> &a, int k)
+  {
+    // code here
+    int n = a.size();
+    int start = 0, end = 0, mid, ans;
+    for (int i = 0; i < n; i++)
+    {
+      start = max(start, a[i]);
+      end += a[i];
+    }
+
+    while (start <= end)
+    {
+      mid = start + (end - start) / 2;
+      int length = 0, count = 1;
+      for (int i = 0; i < n; i++)
+      {
+        length += a[i];
+        if (length > mid)
+        {
+          length = a[i];
+          count++;
+        }
+      }
+
+      if (count <= k)
+      {
+        ans = mid;
+        end = mid - 1;
+      }
+      else
+      {
+        start = mid + 1;
+      }
+    }
+
+    return ans;
+  }
+};
