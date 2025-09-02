ba7c8953477362bcee2f3b1002fd837568e60337
aryan.main27 <aryan.main27@gmail.com>
2025-09-02 16:46:29 +0530

    DSA

---
+++ Allocate-Minimum-Pages.cpp
@@ -0,0 +1,37 @@
+class Solution {
+  public:
+    int findPages(vector<int> &arr, int k) {
+        // code here
+        int n = arr.size();
+        if(n < k)
+        return -1 ;
+        
+        int start = 0 , end = 0 , mid , ans ;
+        for(int i = 0 ; i<n ; i++){
+            start = max(start,arr[i]);
+            end += arr[i];
+        }
+        
+        while(start <= end){
+            mid = start + (end - start)/2 ;
+            int page = 0 , count = 1;
+            for(int i = 0 ; i<n ; i++){
+                page += arr[i];
+                if(page > mid){
+                    count++;
+                    page = arr[i];
+                }
+            }
+            
+            if(count <= k){
+                ans = mid ;
+                end = mid - 1;
+            }
+            else{
+                start = mid + 1 ;
+            }
+        }
+        
+        return ans;
+    }
+};
