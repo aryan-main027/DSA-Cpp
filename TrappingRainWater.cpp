8c0eaa1724d8a84e3666002a9d1f3ba348c77bcd
aryan.main27 <aryan.main27@gmail.com>
2025-09-24 00:31:16 +0530

    Go

---
+++ TrappingRainWater.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+       int n = height.size();
+       int maxleft = 0 , maxright = 0 , water = 0 , maxheight = height[0];
+       int index = 0 ;
+       for(int i = 0 ; i<n ; i++){
+        if(height[i] > maxheight){
+            maxheight = height[i];
+            index = i;
+        }
+       } 
+
+    //    Left 
+       for(int i = 0 ; i<index ; i++){
+        if(maxleft > height[i]){
+            water += (maxleft - height[i]);
+        } else {
+            maxleft = height[i];
+        }
+       }
+
+    //    Right
+       for(int i = n-1 ; i>index ; i--){
+        if(maxright > height[i]){
+            water += (maxright - height[i]);
+        } else {
+            maxright = height[i];
+        }
+       } 
+
+       return water;
+    }
+};
