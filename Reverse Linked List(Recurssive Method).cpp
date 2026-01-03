9cd9b059259ac5a25fa343f5544b71409e363765
aryan.main27 <aryan.main27@gmail.com>
2026-01-03 23:43:28 +0530

    DSA

---
+++ Reverse Linked List(Recurssive Method).cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution
+{
+public:
+  ListNode *Reverse(ListNode *cur, ListNode *prev)
+  {
+    if (cur == NULL)
+      return prev;
+
+    ListNode *fut = cur->next;
+    cur->next = prev;
+    return Reverse(fut, cur);
+  }
+
+  ListNode *reverseList(ListNode *head)
+  {
+    return Reverse(head, NULL);
+  }
+};
